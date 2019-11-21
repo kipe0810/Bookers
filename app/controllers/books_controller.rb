fc63f6f27cb1a3a289b9cf4d8dd0ce30ac185f1c
kipe <tkrs23280810@gmail.com>
2019-11-21 15:12:07 +0900

    [Finish]4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,9 +13,14 @@ class BooksController < ApplicationController
   end
 
   def create
-  	book = Book.new(book_params)
-  	book.save
-  	redirect_to books_path
+  	@book = Book.new(book_params)
+  	if @book.save
+      flash[:notice] = "Book was successfully created."
+  	  redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def edit
@@ -23,14 +28,16 @@ class BooksController < ApplicationController
   end
 
   def update
-  	book = Blog.find(params[:id])
+  	book = Book.find(params[:id])
   	book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
   	redirect_to book_path(book)
   end
 
   def destroy
   	book = Book.find(params[:id])
   	book.destroy
+    flash[:notice] = "Book was successfully destroyed."
   	redirect_to books_path
   end
 
